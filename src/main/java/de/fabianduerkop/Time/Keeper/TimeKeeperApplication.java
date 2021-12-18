19d593a14aa9721c2d02a9b71f15e6b530311a42
Fabian <mail@fabian-duerkop.de>
2021-12-18 20:04:01 +0100

    Spring Boot Initializer

---
+++ src/main/java/de/fabianduerkop/Time/Keeper/TimeKeeperApplication.java
@@ -0,0 +1,13 @@
+package de.fabianduerkop.Time.Keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeKeeperApplication.class, args);
+	}
+
+}
